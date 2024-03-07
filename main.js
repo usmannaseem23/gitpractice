83799ba674757cae2d3af3b8f60eea45a1a8aab2
[Muhammad Usman Naseem] <[usmannaseem2005@gmail.com]>
2024-03-07 09:18:20 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Data Types
+var myname = "Usman";
+myname = "Usman ";
+console.log(myname);
+var username = "Usman Naseem"; // String
+var age = 18; // NUmber 
+var isStudent = false; //Boolean
+var randomvalue = "https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"; //Any 
---
+++ main.ts
@@ -0,0 +1,10 @@
+// Data Types
+let myname = "Usman"
+
+myname = "Usman ";
+console.log(myname);
+
+let username: string = "Usman Naseem"; // String
+let age: number = 18; // NUmber 
+let isStudent: boolean = false ;//Boolean
+let randomvalue : any = "https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"//Any 
